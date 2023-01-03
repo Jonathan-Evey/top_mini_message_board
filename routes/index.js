46b784c9484d969a0124528d3b6fe5b0d294f4ca
Jonathan-Evey <jonathan.c.evey@gmail.com>
2023-01-03 08:15:16 -0800

    removed new rought and message obj from file, and then imported message obj

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-const messages = [
-	{
-		text: 'Hi there!',
-		user: 'Amando',
-		added: new Date(),
-	},
-	{
-		text: 'Hello World!',
-		user: 'Charles',
-		added: new Date(),
-	},
-];
+let messages = require('../data/allPosts');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -20,21 +8,6 @@ router.get('/', function (req, res, next) {
 		title: 'Mini Messageboard',
 		messages: messages,
 	});
-});
-
-router.get('/new', function (req, res, next) {
-	res.render('form', {
-		title: 'Creat Message',
-	});
-});
-
-router.post('/new', function (req, res, next) {
-	messages.unshift({
-		text: req.body.messageText,
-		user: req.body.messageUser,
-		added: new Date(),
-	});
-	res.redirect('/');
 });
 
 module.exports = router;
